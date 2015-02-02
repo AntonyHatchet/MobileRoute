bbee7a1d7092d537a843142aa3758b233aeb61b2
Anton Gorshenin <anton_gorshenin@etb-lab.com>
2015-02-02 19:10:25 +0300

    Добавили карты локально

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,10 +1,18 @@
 $(function() {
 var map=khtml.maplib.Map(document.getElementById("map"));
-map.centerAndZoom(new khtml.maplib.LatLng(0.0,51.2),5);
+var mr = khtml.maplib;
+var p1=new mr.LatLng(55.785381,37.565492);
+var p3=new mr.LatLng(55.791571,37.574075);
+var p2=new mr.LatLng(55.793119,37.616304);
+map.centerAndZoom(new mr.LatLng(55.849154,37.566922),0);
 var zoominger=new khtml.maplib.ui.Zoombar();
 map.addOverlay(zoominger);
-var feature=map.featureCollection.appendChild({type:"LineString",coordinates:[[0,0],[51,0],[100.2,16.5]]});
+var feature=map.featureCollection.appendChild({type:"LineString",coordinates:[p1,p3,p2]});
 feature.className.baseVal="line";
 
-
+    var marker = new khtml.maplib.overlay.Marker({
+        position: new khtml.maplib.LatLng(55.0,37.2),
+        map: map,
+        title:"static marker"
+    });
 });
